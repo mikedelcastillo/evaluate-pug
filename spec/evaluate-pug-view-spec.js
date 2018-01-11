e18f1b7c54a5ddec34312265bae9e74e480bb86b
Mike del Castillo <johnmichaeldc@gmail.com>
2018-01-11 23:00:29 +0800

    init

---
+++ spec/evaluate-pug-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EvaluatePugView from '../lib/evaluate-pug-view';
+
+describe('EvaluatePugView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
